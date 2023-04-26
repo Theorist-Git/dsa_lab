5ac87a024a57fdab37d4a860ae2dffb138c1767f
_Theorist_ <mayankvats460@gmail.com>
2023-04-26 23:45:06 +0530

    static stack implementation with a struct

---
+++ Stack/stack_menu.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "Stack/stack_struct.c"
+
+int main() {
+    stack stk;
+    stk.top = -1;
+
+    while (1) {
+        int choice;
+        printf("Enter 1 to PUSH to stack\nEnter 2 to POP stack\nEnter 3 to SEEK\nEnter 0 to QUIT: ");
+        scanf("%d", &choice);
+        if (choice == 1) {
+            int val;
+            printf("Enter value to be PUSHED: ");
+            scanf("%d", &val);
+            push(&stk, val);
+        } else if (choice == 2) {
+            pop(&stk);
+        } else if (choice == 3) {
+            seek(&stk);
+        } else if (choice == 0) {
+            break;
+        }
+    }
+
+    return 0;
+}
---
+++ Stack/stack_struct.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define MAX 5
+
+typedef struct Stack {
+    int stack[MAX];
+    int top;
+} stack;
+
+int is_full(stack* stk) {
+    if (stk->top == MAX - 1) {
+        return 1;
+    }
+    return 0;
+}
+
+int is_empty(stack* stk) {
+    if (stk->top == -1) {
+        return 1;
+    }
+    return 0;
+}
+
+
+void print_stack(stack* stk) {
+    for (int i = 0; i <= stk->top; i++) {
+        printf("%d ", stk->stack[i]);
+    }
+    printf("\n");
+}
+
+void push(stack* stk, int val) {
+    if (stk->top < MAX - 1) {
+        stk->top++;
+        stk->stack[stk->top] = val;
+        print_stack(stk);
+    } else {
+        printf("Stack Overflow\nMax stack size: %d\n", MAX);
+        print_stack(stk);
+    }
+}
+
+void pop(stack* stk) {
+    if (stk->top != -1) {
+        stk->top--;
+        print_stack(stk);
+    } else {
+        printf("Stack Underflow\n");
+    }
+}
+
+void seek(stack* stk) {
+    if (stk->top != -1) {
+        printf("Last Element i.e top points to: %d", stk->stack[stk->top]);
+    } else {
+        printf("Stack is empty\n");
+    }
+}
