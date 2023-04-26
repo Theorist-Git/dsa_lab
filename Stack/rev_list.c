64aaf4bdbb9cbcaea5d8e3a0c8946c0abc015e7c
_Theorist_ <mayankvats460@gmail.com>
2023-04-27 00:28:41 +0530

    List reversed using stack

---
+++ Stack/rev_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "stack_struct.c"
+
+void input_array(int* arr, int len) {
+    for (int i = 0; i < len; i++) {
+        int num;
+        printf("Enter element for index %d\n", i);
+        scanf("%d", &num);
+        arr[i] = num;
+    }
+}
+
+
+int main() {
+    int arr[10];
+    int len = 10;
+    input_array(arr, len);
+    
+    stack stk;
+    stk.top = -1;
+
+    for (int i = 0; i < len; i++) {
+        push(&stk, arr[len - (i + 1)]);
+    }
+
+    return 0;
+}
